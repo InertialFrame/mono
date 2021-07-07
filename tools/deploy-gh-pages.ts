a2f7d012b7dd9c97fa5b6f313ffae49b9d7f9921
Dorian Thiessen <dorian11thiessen@gmail.com>
2021-07-07 02:24:14 -0600

    Update github pages deployment to include CNAME

---
+++ tools/deploy-gh-pages.ts
@@ -0,0 +1,15 @@
+import * as ghpages from 'gh-pages';
+import * as fs from 'fs';
+
+fs.writeFile(
+	'./dist/apps/gh-pages/CNAME',
+	'mono.inertialframe.ca',
+	function (err) {
+		if (err) return console.error('Error writing CNAME:', err);
+		ghpages.publish(
+			'./dist/apps/gh-pages',
+			{ push: false },
+			(err) => err && console.error('Error publishing:', err)
+		);
+	}
+);
