2617496ca05efe43ce93b5eedae3522c1afd1b1b
Dorian Thiessen <dorian11thiessen@gmail.com>
2021-07-12 18:21:30 -0600

    add sample manifest.json & two icons

--- tools/deploy-gh-pages.ts
+++ tools/deploy-gh-pages.ts
@@ -1,14 +1,14 @@
 import * as ghpages from 'gh-pages';
 import * as fs from 'fs';
 
-fs.writeFile(
-	'./dist/apps/gh-pages/CNAME',
-	'mono.inertialframe.ca',
-	function (err) {
-		if (err) return console.error('Error writing CNAME:', err);
-		ghpages.publish(
-			'./dist/apps/gh-pages',
-			(err) => err && console.error('Error publishing:', err)
-		);
-	}
+fs.writeFileSync('./dist/apps/gh-pages/CNAME', 'mono.inertialframe.ca');
+
+fs.copyFileSync(
+	'./apps/gh-pages/manifest.json',
+	'./dist/apps/gh-pages/manifest.json'
+);
+
+ghpages.publish(
+	'./dist/apps/gh-pages',
+	(err) => err && console.error('Error publishing:', err)
 );
